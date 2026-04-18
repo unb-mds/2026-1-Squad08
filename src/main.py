32c20cce8d420d449bcd66b280f9de0f5d7470a5
Donnk61 <luis.arruda@gmail.com>
2026-04-18 15:20:02 -0400

    implementação inicial com API da câmara dos Deputados

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from services.camara_api import listar_proposicoes
+
+proposicoes = listar_proposicoes()
+
+for proposicao in proposicoes:
+    print(proposicao["id"])
+    print(proposicao["ementa"])
+    print("------")
